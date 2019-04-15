21d244e689e862d5094fb8884ccf3318a96922dc
Shestakov Artem <art.shestakov@icloud.com>
2019-04-15 12:03:19 +0300

    Обновлены комментарии функций и классов.

--- libCrypter/LCGlobal.h
+++ libCrypter/LCGlobal.h
@@ -1,6 +1,21 @@
 #pragma once
 //-----------------------------------------------------------------------------
+
+/**Зашифровать текст в изображение.
+\param PathImage Путь к исходному изображению
+\param PathResult Путь к изображению с зашифрованным текстом
+\param Message Текст
+*/
 extern "C" __declspec(dllexport) int Crypt(const char *PathImage, const char *PathResult, const char *Message);
+
+/**Расшифровать текст из изображения.
+\param PathImage Путь к изображению, содержащему зашифрованный текст
+\return Возвращает текст, зашифрованный в изображение.
+*/
 extern "C" __declspec(dllexport) const char* Decrypt(const char *PathImage);
+
+/**Получить описания ошибки.
+\return Возвращает описание ошибки.
+*/
 extern "C" __declspec(dllexport) const char* GetErrorString();
 //-----------------------------------------------------------------------------
--- libCrypter/LCPixel.h
+++ libCrypter/LCPixel.h
@@ -1,5 +1,6 @@
 #pragma once
 //-----------------------------------------------------------------------------
+//Структура описывающая графический пиксель.
 struct LCPixel
 {
 	LCPixel(const unsigned char &red, const unsigned char &green, const unsigned char &blue, const unsigned char &alpha);
